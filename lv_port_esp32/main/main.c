c5e78d37f743548bc34d864ab8e0ce1bf387ec4b
huangwenguang <huangwenguang@iboxpay.com>
2022-02-24 15:06:03 +0800

    lvgl

--- lv_port_esp32/main/main.c
+++ lv_port_esp32/main/main.c
@@ -29,24 +29,10 @@
 
 #include "lvgl_helpers.h"
 
-#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
-    #if defined CONFIG_LV_USE_DEMO_WIDGETS
-        #include "lv_examples/src/lv_demo_widgets/lv_demo_widgets.h"
-    #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
-        #include "lv_examples/src/lv_demo_keypad_encoder/lv_demo_keypad_encoder.h"
-    #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
-        #include "lv_examples/src/lv_demo_benchmark/lv_demo_benchmark.h"
-    #elif defined CONFIG_LV_USE_DEMO_STRESS
-        #include "lv_examples/src/lv_demo_stress/lv_demo_stress.h"
-    #else
-        #error "No demo application selected."
-    #endif
-#endif
-
 /*********************
  *      DEFINES
  *********************/
-#define TAG "demo"
+#define TAG "clock"
 #define LV_TICK_PERIOD_MS 1
 
 /**********************
@@ -54,32 +40,7 @@
  **********************/
 static void lv_tick_task(void *arg);
 static void guiTask(void *pvParameter);
-static void create_demo_application(void);
-
-static void btn_event_cb(lv_obj_t * btn, lv_event_t event) {
-    if(event == LV_EVENT_CLICKED) {
-        static uint8_t cnt = 0;
-        cnt++;
-
-        /*Get the first child of the button which is the label and change its text*/
-        lv_obj_t * label = lv_obj_get_child(btn, NULL);
-        lv_label_set_text_fmt(label, "Button: %d", cnt);
-    }
-}
-
-/**
- * Create a button with a label and react on Click event.
- */
-void lv_ex_get_started_1(void)
-{
-    lv_obj_t * btn = lv_btn_create(lv_scr_act(), NULL);     /*Add a button the current screen*/
-    lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
-    lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
-    lv_obj_set_event_cb(btn, btn_event_cb);                 /*Assign a callback to the button*/
-
-    lv_obj_t * label = lv_label_create(btn, NULL);          /*Add a label to the button*/
-    lv_label_set_text(label, "Button 1");                     /*Set the labels text*/
-}
+static void func_test();
 
 /**********************
  *   APPLICATION MAIN
@@ -89,7 +50,7 @@ void app_main() {
     /* If you want to use a task to create the graphic, you NEED to create a Pinned task
      * Otherwise there can be problem such as memory corruption and so on.
      * NOTE: When not using Wi-Fi nor Bluetooth you can pin the guiTask to core 0 */
-    xTaskCreatePinnedToCore(guiTask, "gui", 4096*2, NULL, 0, NULL, 1);
+    xTaskCreatePinnedToCore(guiTask, "gui", 4096, NULL, 0, NULL, 1);
 }
 
 /* Creates a semaphore to handle concurrent call to lvgl stuff
@@ -143,8 +104,8 @@ static void guiTask(void *pvParameter) {
     ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
 
     /* Create the demo application */
-    // lv_ex_get_started_1();
-    create_demo_application();
+    // create_demo_application();
+    func_test();
 
     while (1) {
         /* Delay 1 tick (assumes FreeRTOS tick is 10ms */
@@ -165,42 +126,45 @@ static void guiTask(void *pvParameter) {
     vTaskDelete(NULL);
 }
 
-static void create_demo_application(void)
-{
-    /* When using a monochrome display we only show "Hello World" centered on the
-     * screen */
-#if defined CONFIG_LV_TFT_DISPLAY_MONOCHROME || \
-    defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_ST7735S
+LV_IMG_DECLARE(img_cogwheel_argb)
 
-    /* use a pretty small demo for monochrome displays */
-    /* Get the current screen  */
-    lv_obj_t * scr = lv_disp_get_scr_act(NULL);
+static void func_test() {
+    // lv_obj_t * label1 =  lv_label_create(lv_scr_act(), NULL);
+    // lv_label_set_text(label1, "Hello world");
+    // lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
 
-    /*Create a Label on the currently active screen*/
-    lv_obj_t * label1 =  lv_label_create(scr, NULL);
+    // lv_obj_t * btn = lv_btn_create(lv_scr_act(), NULL);
+    // lv_obj_set_pos(btn, 10, 10);
+    // lv_obj_set_size(btn, 120, 50);
+    // lv_obj_t * label = lv_label_create(btn, NULL);
+    // lv_label_set_text(label, "Button 1");
 
-    /*Modify the Label's text*/
-    lv_label_set_text(label1, "Hello\nworld");
+    // lv_obj_t * slider = lv_slider_create(lv_scr_act(), NULL);
+    // lv_obj_set_width(slider, 150); 
+    // lv_obj_align(slider, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, -40);
 
-    /* Align the Label to the center
-     * NULL means align on parent (which is the screen now)
-     * 0, 0 at the end means an x, y offset after alignment*/
-    lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
-#else
-    /* Otherwise we show the selected demo */
+    // label = lv_label_create(lv_scr_act(), NULL);
+    // lv_label_set_text(label, "0");
+    // lv_obj_set_auto_realign(slider, true);
+    // lv_obj_align(label, slider, LV_ALIGN_OUT_BOTTOM_MID, 0, 20);
 
-    #if defined CONFIG_LV_USE_DEMO_WIDGETS
-        lv_demo_widgets();
-    #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
-        lv_demo_keypad_encoder();
-    #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
-        lv_demo_benchmark();
-    #elif defined CONFIG_LV_USE_DEMO_STRESS
-        lv_demo_stress();
-    #else
-        #error "No demo application selected."
-    #endif
-#endif
+    // static lv_style_t style_line;
+    // lv_style_init(&style_line);
+    // lv_style_set_line_width(&style_line, LV_STATE_DEFAULT, 2);
+
+    // static lv_point_t line_points[] = { {0, 70}, {240, 70} };
+    // lv_obj_t * line1;
+    // line1 = lv_line_create(lv_scr_act(), NULL);
+    // lv_line_set_points(line1, line_points, 2);
+    // lv_obj_add_style(line1, LV_LINE_PART_MAIN, &style_line);
+
+    lv_obj_t * img1 = lv_img_create(lv_scr_act(), NULL);
+    lv_img_set_src(img1, &img_cogwheel_argb);
+    lv_obj_align(img1, NULL, LV_ALIGN_CENTER, 0, -20);
+
+    lv_obj_t * img2 = lv_img_create(lv_scr_act(), NULL);
+    lv_img_set_src(img2, LV_SYMBOL_OK "Accept");
+    lv_obj_align(img2, img1, LV_ALIGN_OUT_BOTTOM_MID, 0, 20);
 }
 
 static void lv_tick_task(void *arg) {
